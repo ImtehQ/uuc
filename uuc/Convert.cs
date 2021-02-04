ceec163ffa97b2c65522330e44dff8bb3b4350c6
Perry E <its_Q@hotmail.com>
2021-02-05 00:54:14 +0100

    Fixed some issues

--- uuc/Convert.cs
+++ uuc/Convert.cs
@@ -1,18 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using static uuc.UC_Modules;
+﻿using static uuc.UC_Modules;
 
 namespace uuc
 {
     //Universal Unit Convert, yes it needs a better name!
     public static class uuc
     {
+        /// <summary>
+        /// Converts the value to another unit
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="format"></param>
+        /// <returns></returns>
         public static double Convert(this double value, string format)
         {
             return value.InnerConvert(format);
         }
-
+        /// <summary>
+        /// DO NOT USE
+        /// Its faster but unchecked
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="format"></param>
+        /// <returns></returns>
+        public static double ConvertUnsafe(this double value, string format)
+        {
+            return value.InnerConvertFast(format);
+        }
         /// <summary>
         /// Does the logic to convert the data.
         /// </summary>
@@ -47,10 +60,39 @@ namespace uuc
             double baseValue = UC_Modules.GetBaseValue(fromType, value, fromSetStr);
 
             //Check to see if we need to convert the basevalue to a new type
-            double switchedValue = UC_Modules.SwitchType(fromType, toType, value);
+            double switchedValue = UC_Modules.SwitchType(fromType, toType, baseValue);
 
             //Convert the new base value to the target set.
             return UC_Modules.GetSetValue(toType, switchedValue, toSetStr);
+
+        }
+
+        /// <summary>
+        /// Faster but less useable way of doing it.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="format"></param>
+        /// <returns></returns>
+        private static double InnerConvertFast(this double value, string format)
+        {
+            format = format.Replace(" ", "");
+
+            UnitType fromType = UC_Modules.GetType(format.Substring(0, 1));
+            UnitType toType = UC_Modules.GetType(format.Substring(format.IndexOf(":") + 1, 1));
+
+            return UC_Modules.GetSetValue(
+                toType, 
+                UC_Modules.SwitchType(
+                    fromType, 
+                    toType, 
+                    UC_Modules.GetBaseValue(
+                        fromType, 
+                        value, 
+                        format.Substring(
+                            1, 
+                            format.IndexOf(":") - 1))), 
+                format.Substring((format.IndexOf(":") + 2), 
+                (format.Length - (format.IndexOf(":") + 2))));
 
         }
     }
--- uuc/UC_Modules.cs
+++ uuc/UC_Modules.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace uuc
+﻿namespace uuc
 {
     public static class UC_Modules
     {
